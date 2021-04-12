10db2bf79e116a7dea0e867459b7295a6822feca
gssaini117 <gssaini@ucsc.edu>
2021-04-12 01:17:35 -0700

    Added Explosions :D

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -35,4 +35,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.y = game.config.height - borderUISize - borderPadding;
         }
     }
+
+    reset() { // resets rocket
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
 }
